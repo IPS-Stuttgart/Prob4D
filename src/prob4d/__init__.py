6c9afcbbb181654e73d965c7fd70d1e29cb4646c
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-26 17:04:46 +0200

    Export observation factor contracts

--- src/prob4d/__init__.py
+++ src/prob4d/__init__.py
@@ -1,7 +1,26 @@
 """Probabilistic long-horizon fusion for MotionCrafter predictions."""
 
 from .data import PredictionWindow
+from .observation_factors import (
+    LinearizedObservationFactor,
+    ObservationFactor,
+    ObservationFactorBundle,
+    StackedObservationFactors,
+    load_observation_factor_bundle,
+    stack_observation_factors,
+    write_observation_factor_bundle,
+)
 from .sim3 import Sim3
 
-__all__ = ["PredictionWindow", "Sim3"]
+__all__ = [
+    "LinearizedObservationFactor",
+    "ObservationFactor",
+    "ObservationFactorBundle",
+    "PredictionWindow",
+    "Sim3",
+    "StackedObservationFactors",
+    "load_observation_factor_bundle",
+    "stack_observation_factors",
+    "write_observation_factor_bundle",
+]
 __version__ = "0.1.0"
