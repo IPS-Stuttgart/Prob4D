a0abead89a0a643cbd7bfb36683641d7329dd1e0
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-26 17:01:01 +0200

    Export observation artifact APIs

--- src/prob4d/__init__.py
+++ src/prob4d/__init__.py
@@ -1,7 +1,16 @@
 """Probabilistic long-horizon fusion for MotionCrafter predictions."""
 
 from .data import PredictionWindow
+from .observation import ObservationArtifact, SourceWindowProvenance
+from .observation_io import load_observation_artifact, save_observation_artifact
 from .sim3 import Sim3
 
-__all__ = ["PredictionWindow", "Sim3"]
+__all__ = [
+    "ObservationArtifact",
+    "PredictionWindow",
+    "Sim3",
+    "SourceWindowProvenance",
+    "load_observation_artifact",
+    "save_observation_artifact",
+]
 __version__ = "0.1.0"
