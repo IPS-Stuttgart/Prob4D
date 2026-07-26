17c7927cd4751b8157c80e6bf0c296b2d6c1cd56
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-26 17:05:27 +0200

    Export observation contract API

--- src/prob4d/__init__.py
+++ src/prob4d/__init__.py
@@ -1,7 +1,20 @@
 """Probabilistic long-horizon fusion for MotionCrafter predictions."""
 
 from .data import PredictionWindow
+from .observation_contract import (
+    OBSERVATION_BELIEF_SCHEMA,
+    OBSERVATION_BELIEF_VERSION,
+    ObservationBeliefExportV1,
+    save_observation_belief_export,
+)
 from .sim3 import Sim3
 
-__all__ = ["PredictionWindow", "Sim3"]
+__all__ = [
+    "OBSERVATION_BELIEF_SCHEMA",
+    "OBSERVATION_BELIEF_VERSION",
+    "ObservationBeliefExportV1",
+    "PredictionWindow",
+    "Sim3",
+    "save_observation_belief_export",
+]
 __version__ = "0.1.0"
