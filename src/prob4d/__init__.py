b29bf36165a88486f7c4ad827c8a83b14198191c
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-26 20:09:04 +0200

    Expose source-causal observation belief APIs

--- src/prob4d/__init__.py
+++ src/prob4d/__init__.py
@@ -1,6 +1,17 @@
 """Probabilistic long-horizon fusion for MotionCrafter predictions."""
 
 from .data import PredictionWindow
+from .observation_contract import (
+    OBSERVATION_BELIEF_SCHEMA,
+    OBSERVATION_BELIEF_VERSION,
+    ObservationBeliefExportV1,
+    save_observation_belief_export,
+)
+from .observation_export import (
+    admit_source_causal_windows,
+    build_prob4d_observation_belief,
+    gauge_covariance_factor,
+)
 from .observation_factors import (
     LinearizedObservationFactor,
     ObservationFactor,
@@ -13,14 +24,21 @@ from .observation_factors import (
 from .sim3 import Sim3
 
 __all__ = [
+    "OBSERVATION_BELIEF_SCHEMA",
+    "OBSERVATION_BELIEF_VERSION",
     "LinearizedObservationFactor",
+    "ObservationBeliefExportV1",
     "ObservationFactor",
     "ObservationFactorBundle",
     "PredictionWindow",
     "Sim3",
     "StackedObservationFactors",
+    "admit_source_causal_windows",
+    "build_prob4d_observation_belief",
+    "gauge_covariance_factor",
     "load_observation_factor_bundle",
+    "save_observation_belief_export",
     "stack_observation_factors",
     "write_observation_factor_bundle",
 ]
-__version__ = "0.1.0"
+__version__ = "0.2.0"
